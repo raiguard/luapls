0e65305e585591e238eb63b8ac613fdb8a7e1588
Caleb Heuer <ch@raiguard.me>
2023-11-27 21:35:07 -0700

    Move textDocumentHover to a dedicated file

--- lsp/document.go
+++ lsp/document.go
@@ -42,31 +42,6 @@ func textDocumentHighlight(ctx *glsp.Context, params *protocol.DocumentHighlight
 		{Range: file.ToProtocolRange(ast.Range(node))},
 	}, nil
 }
-
-func textDocumentHover(ctx *glsp.Context, params *protocol.HoverParams) (*protocol.Hover, error) {
-	file := files[params.TextDocument.URI]
-	if file == nil {
-		return nil, nil
-	}
-	node, _ := ast.GetNode(&file.Block, file.ToPos(params.Position))
-	if node == nil {
-		return nil, nil
-	}
-	return &protocol.Hover{
-		Contents: fmt.Sprintf(
-			"# %T\n\nRange: `{%d, %d, %d}` `{%d, %d, %d}`",
-			node,
-			file.ToProtocolRange(ast.Range(node)).Start.Line,
-			file.ToProtocolRange(ast.Range(node)).Start.Character,
-			node.Pos(),
-			file.ToProtocolRange(ast.Range(node)).End.Line,
-			file.ToProtocolRange(ast.Range(node)).End.Character,
-			node.End(),
-		),
-		Range: ptr(file.ToProtocolRange(ast.Range(node))),
-	}, nil
-}
-
 func textDocumentSelectionRange(ctx *glsp.Context, params *protocol.SelectionRangeParams) ([]protocol.SelectionRange, error) {
 	file := files[params.TextDocument.URI]
 	if file == nil {
